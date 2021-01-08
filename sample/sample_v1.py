492cbfcc63af929f2f9e18f38926c328e3dc1325
TabrisXiao <tabris.xiao@gmail.com>
2021-01-08 14:16:24 -0600

    normalized sample generator

--- sample/sample_v1.py
+++ sample/sample_v1.py
@@ -2,6 +2,9 @@
 import numpy as np
 from dataWrapper import data_wrapper as drp
 
+#the pixel value for the image are ranging over [0,1], so for any 8bit gray scale images, it should be normalized to this scale range for training convenience.
+
+
 def randomPoint(size, shift= [0,0]):
 	x = shift[0]+int(np.random.randint(size[0]-2*shift[0]-1))
 	y = shift[1]+int(np.random.randint(size[1]-2*shift[1]-1))
@@ -57,7 +60,7 @@ def bkg_sample_v1(point, size,dtype, bkg_noise, bkg_obj):
 	do_shape = np.random.uniform(0,1)
 	#if do_shape < 0.3: return bkg
 	rad = int(np.random.uniform(10,60))
-	val = np.random.uniform(0.6,6)
+	val = np.random.uniform(0.1,1)
 	
 	start = randomPoint(size, point)
 	if bkg_obj:
@@ -72,9 +75,7 @@ def signal_gen(point, r, size, sig_f, value, dtype):
 
 def signal_motion_sample_v1(r,size, shift,ntime, dtype, **kwargs):
 	reverseRate = 0
-	bkgScale = [0.9, 1.1]
 	keys = kwargs.keys()
-	if "bkgScale" in keys: bkgScale = kwargs["bkgScale"]
 	if "bkg_noise" in keys: bkg_noise = True
 	else : bkg_noise = False
 	if "bkg_obj" in kwargs.keys(): bkg_obj = True
@@ -84,10 +85,10 @@ def signal_motion_sample_v1(r,size, shift,ntime, dtype, **kwargs):
 	y = shift[1]+int(np.random.randint(size[1]-2*shift[1]-1))
 	
 	bkg = bkg_sample_v1(shift, size, dtype, bkg_noise,bkg_obj)
-	scale = np.random.uniform(bkgScale[0],bkgScale[1])
-	bkg = np.multiply(bkg,scale)
 	#bkg = bkg_sample(size, dtype)
 	reverseDice = np.random.uniform(0,1)
+	scale = np.random.uniform(0.2,1)
+	bkg = np.multiply(bkg,scale)
 	 
 	vmax_x = size[0]//20
 	vmax_y = size[1]//20
@@ -100,12 +101,11 @@ def signal_motion_sample_v1(r,size, shift,ntime, dtype, **kwargs):
 	sig_f = dice_draw_map()
 	val = np.random.uniform(0.2,1)
 	for i in range(ntime):
-		if reverseDice < reverseRate:
-			d = signal_gen([x,y], r, size, sig_f, -val, dtype)
-		else:
-			d = signal_gen([x,y], r, size, sig_f, val, dtype)
+		d = signal_gen([x,y], r, size, sig_f, val, dtype)
 		d0 = signal_gen([x,y], r, size, sig_f, 1, dtype)
-		data.append(np.add(d,bkg))
+		if reverseDice < reverseRate: d = np.add(d,bkg)
+		else: d = np.subtract(bkg,d)
+		data.append(np.clip(d,0, 1))
 		sig.append(d0[shift[0]:size[0]-shift[0], shift[1]:size[1]-shift[1]])
 		dx = int(np.random.randint(-vmax_x,vmax_x))
 		dy = int(np.random.randint(-vmax_y,vmax_y))
